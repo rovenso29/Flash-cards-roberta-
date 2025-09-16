16423c824ce0b9023d7004418680a94c6197e4a4
rovenso29 <roberta.venso@escola.pr.gov.br>
2025-09-16 10:36:27 -0300

    16-09

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
-    let cartão = document.childElemen('article');
-    cartão.className = 'cartão'
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao'
 
-    cartão.innerHTML = '
-< div class="cartao__conteudo" >
-<h3>$(categoria)</h3>
-<div class="cartao__conteudo__pergunta">
-<p>$(pergunta)</p>
-</div>
-<div class="cartao__conteudo__resposta">
-<p>$(resposta)</p>
-</div>
-</div >'
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+    <p>${resposta}</p>
+    </div>
+    </div>`
 
-container.appendChild(cartão)
-
-
-
+    container.appendChild(cartao)
 
 }
