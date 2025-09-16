7dcbc62629b2d7f0b9e2ebaf87cfbe5ecfab18e8
rovenso29 <roberta.venso@escola.pr.gov.br>
2025-09-16 10:31:26 -0300

    16-09

--- Perguntas.js
+++ Perguntas.js
@@ -9,3 +9,60 @@ criaCartao(
     'O que é Python',
     'O python é uma linguagem de programação'
 )
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde se localiza Machu Picchu?',
+    'Peru'
+)
+
+criaCartao(
+    'Geografia',
+    'Que país tem o formato de uma bota?',
+    'Itália'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o monumento famoso pela sua inclinação?',
+    'Torre de Pisa'
+)
+
+criaCartao(
+    'Historia',
+    'Quem pintou Mona Lisa?',
+    'Leonardo Da Vinci'
+)
+criaCartao(
+    'Química',
+    'Qual o nome popular do cloreto de sódio?',
+    'Sal de cozinha'
+)
+criaCartao(
+    'Esportes',
+    'Depois do futebol, qual o esporte mais popular no Brasil?'
+    , 'Volei'
+)
+criaCartao(
+    'Conhecimentos Gerais',
+    'Que navio famoso naufragou na sua viagem inaugural?'
+    , 'Titanic'
+)
+criaCartao(
+    'Ciências',
+    'Qual o satélite natural da Terra?',
+    'Lua'
+)
+criaCartao(
+    'Historia',
+    'Que grande evento histórico aconteceu em 1822 no Brasil?',
+    'Independência do Brasil'
+)
+
+
