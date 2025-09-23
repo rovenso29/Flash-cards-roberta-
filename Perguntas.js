d036faee00b7f54b8d55f78e9b58d18374010e00
rovenso29 <roberta.venso@escola.pr.gov.br>
2025-09-23 09:20:06 -0300

    23-09

--- Perguntas.js
+++ Perguntas.js
@@ -39,30 +39,36 @@ criaCartao(
     'Quem pintou Mona Lisa?',
     'Leonardo Da Vinci'
 )
+
 criaCartao(
     'Química',
     'Qual o nome popular do cloreto de sódio?',
     'Sal de cozinha'
 )
+
 criaCartao(
     'Esportes',
     'Depois do futebol, qual o esporte mais popular no Brasil?'
     , 'Volei'
 )
+
 criaCartao(
     'Conhecimentos Gerais',
     'Que navio famoso naufragou na sua viagem inaugural?'
     , 'Titanic'
 )
+
 criaCartao(
     'Ciências',
     'Qual o satélite natural da Terra?',
     'Lua'
 )
+
 criaCartao(
     'Historia',
     'Que grande evento histórico aconteceu em 1822 no Brasil?',
     'Independência do Brasil'
+
 )
 
 
