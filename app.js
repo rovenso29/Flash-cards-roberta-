8569e8fa5a0a2f15564ed159b2028336361c8972
rovenso29 <roberta.venso@escola.pr.gov.br>
2025-09-23 10:33:40 -0300

    23-09

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>`
 
-let respostaEstaVisivel = false
+    let respostaEstaVisivel = false
 
-function viraCartao(){
-    respostaEstaVisivel=!respostaEstaVisivel
-}
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active',respostaEstaVisivel)
+    }
 
-
-
+    cartao.addEventListener('click',viraCartao)
+    
     container.appendChild(cartao)
 
 }
